6a025b7545c2619264daa24b77556e24ec8de702
Dzuy Pham <dzuyp@juniper.net>
2019-02-25 14:37:02 +1100

    Fixed cli command behaviour

--- Executor/lib/junos_cli_trigger.py
+++ Executor/lib/junos_cli_trigger.py
@@ -1,7 +1,17 @@
 import logging
+import warnings
+import re
+
+import cryptography
 import yaml
+from cryptography import utils
 from jnpr.junos import Device
 from jnpr.junos.exception import ConnectError
+from jnpr.junos.utils.start_shell import StartShell
+
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore', cryptography.utils)
+    import cryptography.hazmat.primitives.constant_time
 
 logger = logging.getLogger(__name__)
 
@@ -42,7 +52,19 @@ class JunosCliTrigger(object):
 
         dev = self.connected_devices[args]
         logger.info('Executing CLI command: %s', command)
-        output = dev.cli(command)
+
+        with StartShell(dev) as ss:
+            _, output = ss.run(r'cli -c "{} | no-more"'.format(command), timeout=30)
         if output:
             logger.info('Success')
+            output = re.sub('\\r\\n---\(more\s\d+%\)---\\r\s+\r', '\n', output)
+            output = output.replace('\r', '')
         return output
+
+if __name__ == '__main__':
+    jc = JunosCliTrigger('../config/devices.yaml')
+    output = jc.execute({
+        'cmd': 'show log messages | match interface | last 100',
+        'args': 'P1'
+    })
+    print(repr(output))
