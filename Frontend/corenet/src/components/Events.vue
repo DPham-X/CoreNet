5d30582ebb6e4736a9c11b70cf28ad7519325bc7
Dzuy Pham <dzuyp@juniper.net>
2019-03-15 11:24:15 +1100

    Added a default local IP that is expected to be changed when deployed

--- Frontend/corenet/src/components/Events.vue
+++ Frontend/corenet/src/components/Events.vue
@@ -128,7 +128,7 @@ export default {
       if (this.status === false) {
         return
       }
-      const link = 'http://10.49.227.135:5000/'
+      const link = 'http://0.0.0.0:5000/'
       const apiLink = link + 'get_events_last'
 
       axios
