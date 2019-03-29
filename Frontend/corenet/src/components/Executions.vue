0ab9384710b9d30fab69842ad50aa5aeb557114a
Dzuy Pham <dzuyp@juniper.net>
2019-03-29 11:17:06 +1100

    Fixed a bug where the output was going off the page by making it scrollable

--- Frontend/corenet/src/components/Executions.vue
+++ Frontend/corenet/src/components/Executions.vue
@@ -52,7 +52,7 @@
           </span>
         </b-table>
 
-        <b-modal id="cmdModal" title="Executed command output" size="xl" class="font-black" style="overflow-x:auto; max-width: 100vh">
+        <b-modal id="cmdModal" title="Executed command output" size="xl" class="font-black" scrollable=true style="overflow-x:auto; max-width: 100vh">
           <div class="font-monospace" v-html="commandOutput"></div>
         </b-modal>
 
